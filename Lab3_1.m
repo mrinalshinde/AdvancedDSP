4b35d95790928d1cbb69758f752cc55a2815c96c
Mrinal Shinde <mrinalgoa@gmail.com>
2018-12-06 15:29:31 +0100

    autocorrelation lab 3_1 done

--- Lab3_1.m
+++ Lab3_1.m
@@ -1,59 +1,38 @@
-clear
+clear vars
+clear all
+close all
+clc
 randn('state',0);
-N=100;
-X=randn(1,N); % AWGN noise 
-R=1024;
-[RB,LB]=xcorr(X,'biased');
-BIAS=xcorr(X,'biased');
-[RUB,LUB]=xcorr(X,'unbiased');
-figure(3)
-subplot(2,2,1)
-stem(LB,RB);
-xlabel('Lag');
-ylabel('Correlation biased');
+noise = randn(1,100);
+[acs_biased,~] = xcorr(noise,'biased');
+[acs_unbiased,n] = xcorr(noise,'unbiased');
+figure()
 
-subplot(2,2,2)
-stem(LUB,RUB);
-xlabel('Lag');
-ylabel('Correlation unbiased');
+subplot(2,2,1);
+stem(n, acs_biased)
+title('')
+ylabel ('amplitude')
+xlabel ('lag index')
+title('Biased ACS')
 
+subplot(2,2,2);
+stem(n, acs_unbiased)
+ylabel ('amplitude')
+xlabel ('lag index');
+title('Unbiased ACS')
 
-%ZB=fft(Z);
-%ZB=fftshift(ZB);
-%normFreq=(-N+1:N-1)/N;
-%k=1:199;
-%W=exp((-2*pi*1i*k)/N);
-%lag=W./pi;
-%ZUB=(fft(W)).^2/2*N;
-%ZUB=fft(RUB);
-%ZUB=fftshift(ZUB);
-%subplot(2,2,3);
-%plot(normFreq,10*log10(ZB),normFreq,10*log10(ZUB)); 
-
-%k1=15;
-%K=1:15;
-
-%M=ZB./N;
-%MS=ZB.^2/N;
-
-%VAR=cov(ZB);
-%subplot(2,2,4);
-%plot(K,VAR);
-
-subplot(2,2,3);
-BPSD=abs(fft(RB,R));
-W = 0:2*pi/R:2*pi*(1-1/R);
-line(W/pi,10*log10(BPSD)); 
-ylim([-50 30]);
-xlim([0 1]);
-grid on;
-xlabel('Normalized Frequency  (\times\pi rad/sample)');
-ylabel('Power/frequency (dB/rad/sample)');
-title('PSD ');
-hold on;
-
-UBPSD=abs(fft(RUB,R));
-line(W/pi,10*log10(UBPSD),'color',[1 0 0]); 
+subplot(2,2,3:4);
+phi_biased = fft(acs_biased,512);
+phi_unbiased = fft(acs_unbiased,512);
+faxis = 0 : 2*pi/512 : 2*pi*(1-1/512);
+hold on
+line(faxis/pi,abs(phi_biased)); 
+line(faxis/pi,abs(phi_unbiased),'color',[1 0 0]);
+grid on
+ylabel ('|\Phi_{XX}(e^{j\omega})|')
+xlabel ('\omega/pi');
+title('Power Spectral Density')
 legend('Biased','Unbiased');
-
+set(gca, 'YScale', 'log')
+hold off
 
