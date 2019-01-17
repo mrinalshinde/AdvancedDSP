893dd98a95ff9f11cbb3c79eab1fe2789309ccc8
Mrinal Shinde <mrinalgoa@gmail.com>
2019-01-17 15:45:08 +0100

    lab 5

--- dsplab5/Lab5_2.m
+++ dsplab5/Lab5_2.m
@@ -3,10 +3,10 @@ close all
 clc
 G=1;
 
-SOS=[1.0000000, 2.0000000, 1.0000000, 1.0000000, -0.6762858, 0.3917468]; 
+SOS = [1.0000000, 2.0000000, 1.0000000, 1.0000000, -0.6762858, 0.3917468]; 
 hd = dfilt.df2sos(SOS,G);
 set(hd, 'Arithmetic', 'fixed');
-[L2_norm]=cascadescale(SOS,2);
+[L2_norm] = cascadescale(SOS,2);
 %disp('L2_norm');
 %disp(L2_norm);
 
@@ -14,6 +14,7 @@ set(hd, 'Arithmetic', 'fixed');
  L2=scale(hd,'l2');
 % fvtool(hd);
 % realizemdl(hd);
+ fprintf('\n')
  disp(L2);
 
 
--- dsplab5/lab5_1.m
+++ dsplab5/lab5_1.m
@@ -7,8 +7,7 @@ L=12;
 SOS=[0.0662272, 0.0662272,0.0000000,1.0000000, -0.2593284, 0.0000000;...
 1.0000000, 2.0000000, 1.0000000, 1.0000000, -0.6762858, 0.3917468];
 [L2_norm]=cascadescale(SOS,3);
-disp('L2_norm');
-disp(L2_norm);
+fprintf('\nL2_norm = %f',L2_norm);
 % %hd = dfilt.df2sos(SOS,G);
 % %set(hd, 'Arithmetic', 'fixed', ... 
 %      'CoeffWordLength', B, ...
